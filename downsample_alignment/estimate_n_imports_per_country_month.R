41e49347ba530c8f6ad37e988d908979faf3c9c0
Sarah Nadeau <sarah.nadeau@bsse.ethz.ch>
2021-01-19 15:04:54 +0100

    write out metadata.txt only if no context sequences

--- downsample_alignment/estimate_n_imports_per_country_month.R
+++ downsample_alignment/estimate_n_imports_per_country_month.R
@@ -22,6 +22,7 @@ parser$add_argument("--metadata", type="character")
 parser$add_argument("--swissseqs", type="character")
 parser$add_argument("--outdirdata", type="character")
 parser$add_argument("--outdirfigs", type="character")
+parser$add_argument("--approxncontextseqs", type="integer")
 
 args <- parser$parse_args()
 
@@ -32,6 +33,7 @@ METADATA_RAW <- args$metadata
 SWISS_SEQS <- args$swissseqs
 OUTDIR_DATA <- args$outdirdata
 OUTDIR <- args$outdirfigs
+N_CONTEXT_SEQS <- args$approxncontextseqs
 
 per_infectious_pop <- read.delim(file = PER_INFECTIOUS_POP_DATA, stringsAsFactors = F)
 travel <- read.delim(file = TRAVEL_DATA, stringsAsFactors = F)
@@ -39,9 +41,11 @@ priorities <- read.delim(file = PRIORITY_INFO, stringsAsFactors = F, header = F)
 metadata_raw <- read.delim(file = METADATA_RAW, stringsAsFactors = F) 
 swiss_seqs <- ape::read.FASTA(file = SWISS_SEQS)
 
-countries_to_plot <- c("Italy", "Germany", "France", "United Kingdom", "United States", "Belgium", "Austria", "Netherlands")
+countries_to_plot <- c("Italy", "Germany", "France", "United Kingdom", 
+                       "United States", "Belgium", "Austria", "Netherlands")
 color_scale <- c(
-  RColorBrewer::brewer.pal(n = length(countries_to_plot), name = "Paired"), "brown")
+  RColorBrewer::brewer.pal(n = length(countries_to_plot), name = "Paired"), 
+  "brown")
 names(color_scale) <- c(countries_to_plot, "other")
 
 # Filter metadata to context sequence set and double-check no bad dates (should be filtered out during QC anyways)
@@ -66,114 +70,116 @@ write.table(
   file = paste(OUTDIR_DATA, "metadata_all.txt", sep = "/"),
   row.names = F, col.names = T, quote = F, sep = "\t")
 
-travel_final <- travel %>% 
-  mutate(
-    year_month = paste(year, month, sep = "-"),
-    country_recoded = recode(
-      .x = source_country,
-      Czechia = "Czech Republic",
-      `Taiwan  Chinese Taipei ` = "Taiwan",
-      Irland = "Ireland",
-      `New Zealand  Oceania` = "New Zealand",
-      `Hong Kong` = "China")) %>%
-  group_by(country_recoded, year_month) %>%
-  summarise(n_arrivals = sum(n_arrivals))
-
-per_infectious_pop_2 <- per_infectious_pop %>% 
-  mutate(
-    country_recoded = gsub(x = country, pattern = "_", replacement = " ")) %>%
-  mutate(
-    country_recoded = recode(
-      .x = country_recoded,
-      Burma = "Myanmar",
-      Czechia = "Czech Republic")) %>%
-  select(-c(country))
-
-per_infectious_pop_final <- per_infectious_pop_2 %>%
-  complete(expand(per_infectious_pop_2, country_recoded, year_month)) %>%
-  mutate(
-    avg_per_pop_infectious = replace_na(
-      data = avg_per_pop_infectious,
-      replace = 0))
-
-# Check data completeness
-print("Locations with arrivals into CH but no sequences:")
-travel_final %>%
-  filter(
-    n_arrivals > 0, 
-    !(country_recoded %in% metadata_final$country_recoded)) %>%
-  group_by(country_recoded) %>%
-  summarize(n_arrivals = sum(n_arrivals))
-
-print("Locations with arrivals into CH but no information of source country infection load:")
-travel_final %>%
-  filter(
-    n_arrivals > 0,
-    !(country_recoded %in% per_infectious_pop_final$country_recoded)) %>%
-  group_by(country_recoded) %>%
-  summarize(n_arrivals = sum(n_arrivals))
-
-# Get estimated # imports from each country in each month
-import_data <- merge(
-  x = travel_final, 
-  y = per_infectious_pop_final, 
-  all.x = T)
-import_data_final <- import_data %>%
-  mutate(est_n_imports = n_arrivals * avg_per_pop_infectious)
-
-write.table(
-  x = import_data_final,
-  file = paste(OUTDIR_DATA, "estimated_imports_per_country_month.txt", sep = "/"),
-  sep = "\t", row.names = F, col.names = T, quote = F)
-
-# Plot estimated import cases over time for sanity-check & supplemental
-early_timeframe <- "Dec. 2019 -\n Feb. 2020"
-late_timeframe <- "Mar. 2020 - Sept. 2020"
-import_data_to_plot <- import_data_final %>%
-  mutate(
-    source_location = case_when(
-      country_recoded %in% countries_to_plot ~ country_recoded,
-      T ~ "other"),
-    timeframe = case_when(
-      year_month %in% c("2019-12", "2020-1", "2020-2") ~ early_timeframe,
-      T ~ late_timeframe))
-
-print(paste("estimated", sum(import_data_to_plot$est_n_imports, na.rm = T), "total imports."))
-
-import_data_to_plot$source_location <- factor(
-  x = import_data_to_plot$source_location,
-  levels = c(countries_to_plot, "other"))
-
-import_data_to_plot$timeframe <- factor(
-  x = import_data_to_plot$timeframe,
-  levels = c(early_timeframe, late_timeframe))
-
-p <- ggplot(
-  data = import_data_to_plot,
-  aes(x = year_month, y = est_n_imports)) +
-  geom_col(aes(fill = source_location)) + 
-  theme_bw() + 
-  labs(x = element_blank(), y = "Estimated No. individuals") + 
-  facet_wrap(. ~ timeframe, scales = "free") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
-  scale_fill_manual(values = color_scale)
-
-# From: https://stackoverflow.com/questions/52341385/how-to-automatically-adjust-the-width-of-each-facet-for-facet-wrap/52422707
-gp <- ggplotGrob(p)
-
-# get gtable columns corresponding to the facets (5 & 9, in this case)
-facet.columns <- gp$layout$l[grepl("panel", gp$layout$name)]
-
-# get the number of unique x-axis values per facet (1 & 3, in this case)
-x.var <- sapply(ggplot_build(p)$layout$panel_scales_x,
-                function(l) length(l$range$range))
-
-# change the relative widths of the facet columns based on
-# how many unique x-axis values are in each facet
-gp$widths[facet.columns] <- gp$widths[facet.columns] * x.var
-
-png(
-  file = paste(OUTDIR, "estimated_imports_per_country_month.png", sep = "/"), 
-  width = 5, height = 3, units = "in", res = 300)
-grid::grid.draw(gp)
-dev.off()
+if (N_CONTEXT_SEQS > 0) {
+  travel_final <- travel %>% 
+    mutate(
+      year_month = paste(year, month, sep = "-"),
+      country_recoded = recode(
+        .x = source_country,
+        Czechia = "Czech Republic",
+        `Taiwan  Chinese Taipei ` = "Taiwan",
+        Irland = "Ireland",
+        `New Zealand  Oceania` = "New Zealand",
+        `Hong Kong` = "China")) %>%
+    group_by(country_recoded, year_month) %>%
+    summarise(n_arrivals = sum(n_arrivals))
+  
+  per_infectious_pop_2 <- per_infectious_pop %>% 
+    mutate(
+      country_recoded = gsub(x = country, pattern = "_", replacement = " ")) %>%
+    mutate(
+      country_recoded = recode(
+        .x = country_recoded,
+        Burma = "Myanmar",
+        Czechia = "Czech Republic")) %>%
+    select(-c(country))
+  
+  per_infectious_pop_final <- per_infectious_pop_2 %>%
+    complete(expand(per_infectious_pop_2, country_recoded, year_month)) %>%
+    mutate(
+      avg_per_pop_infectious = replace_na(
+        data = avg_per_pop_infectious,
+        replace = 0))
+  
+  # Check data completeness
+  print("Locations with arrivals into CH but no sequences:")
+  travel_final %>%
+    filter(
+      n_arrivals > 0, 
+      !(country_recoded %in% metadata_final$country_recoded)) %>%
+    group_by(country_recoded) %>%
+    summarize(n_arrivals = sum(n_arrivals))
+  
+  print("Locations with arrivals into CH but no information of source country infection load:")
+  travel_final %>%
+    filter(
+      n_arrivals > 0,
+      !(country_recoded %in% per_infectious_pop_final$country_recoded)) %>%
+    group_by(country_recoded) %>%
+    summarize(n_arrivals = sum(n_arrivals))
+  
+  # Get estimated # imports from each country in each month
+  import_data <- merge(
+    x = travel_final, 
+    y = per_infectious_pop_final, 
+    all.x = T)
+  import_data_final <- import_data %>%
+    mutate(est_n_imports = n_arrivals * avg_per_pop_infectious)
+  
+  write.table(
+    x = import_data_final,
+    file = paste(OUTDIR_DATA, "estimated_imports_per_country_month.txt", sep = "/"),
+    sep = "\t", row.names = F, col.names = T, quote = F)
+  
+  # Plot estimated import cases over time for sanity-check & supplemental
+  early_timeframe <- "Dec. 2019 -\n Feb. 2020"
+  late_timeframe <- "Mar. 2020 - Sept. 2020"
+  import_data_to_plot <- import_data_final %>%
+    mutate(
+      source_location = case_when(
+        country_recoded %in% countries_to_plot ~ country_recoded,
+        T ~ "other"),
+      timeframe = case_when(
+        year_month %in% c("2019-12", "2020-1", "2020-2") ~ early_timeframe,
+        T ~ late_timeframe))
+  
+  print(paste("estimated", sum(import_data_to_plot$est_n_imports, na.rm = T), "total imports."))
+  
+  import_data_to_plot$source_location <- factor(
+    x = import_data_to_plot$source_location,
+    levels = c(countries_to_plot, "other"))
+  
+  import_data_to_plot$timeframe <- factor(
+    x = import_data_to_plot$timeframe,
+    levels = c(early_timeframe, late_timeframe))
+  
+  p <- ggplot(
+    data = import_data_to_plot,
+    aes(x = year_month, y = est_n_imports)) +
+    geom_col(aes(fill = source_location)) + 
+    theme_bw() + 
+    labs(x = element_blank(), y = "Estimated No. individuals") + 
+    facet_wrap(. ~ timeframe, scales = "free") +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) + 
+    scale_fill_manual(values = color_scale)
+  
+  # From: https://stackoverflow.com/questions/52341385/how-to-automatically-adjust-the-width-of-each-facet-for-facet-wrap/52422707
+  gp <- ggplotGrob(p)
+  
+  # get gtable columns corresponding to the facets (5 & 9, in this case)
+  facet.columns <- gp$layout$l[grepl("panel", gp$layout$name)]
+  
+  # get the number of unique x-axis values per facet (1 & 3, in this case)
+  x.var <- sapply(ggplot_build(p)$layout$panel_scales_x,
+                  function(l) length(l$range$range))
+  
+  # change the relative widths of the facet columns based on
+  # how many unique x-axis values are in each facet
+  gp$widths[facet.columns] <- gp$widths[facet.columns] * x.var
+  
+  png(
+    file = paste(OUTDIR, "estimated_imports_per_country_month.png", sep = "/"), 
+    width = 5, height = 3, units = "in", res = 300)
+  grid::grid.draw(gp)
+  dev.off()
+}
